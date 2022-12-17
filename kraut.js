cc257e8c168feb82fdb7fc3834d04fc4a20f30a8
gayathri-kayapati <gayathrikayapati@gmail.com>
2022-12-17 11:20:17 +0530

    js: added translated functionality.

--- kraut.js
+++ kraut.js
@@ -1,4 +1,9 @@
 var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
-var output = document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
 var serverUrl = "https://api.funtranslations.com/translate/kraut.json";
+
+function getTranslationUrl(text) {
+  return serverUrl + "?" + "text= " + text;
+}
+btnTranslate.addEventListener("click", btnClickHandler);
