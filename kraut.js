4d7595581c433b461ec7e4c82223c1006684c2f3
gayathri-kayapati <gayathrikayapati@gmail.com>
2022-12-17 09:45:19 +0530

    js:reference given to the button textarea and output

---
+++ kraut.js
@@ -0,0 +1,4 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var output = document.querySelector("#output");
+var serverUrl = "https://api.funtranslations.com/translate/kraut.json";
