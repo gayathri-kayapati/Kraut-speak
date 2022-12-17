efc95bd110edbb558edec2355307b698d5c50ba7
gayathri-kayapati <gayathrikayapati@gmail.com>
2022-12-17 11:21:46 +0530

    js:added button clickhandlerfunctionality

--- kraut.js
+++ kraut.js
@@ -6,4 +6,13 @@ var serverUrl = "https://api.funtranslations.com/translate/kraut.json";
 function getTranslationUrl(text) {
   return serverUrl + "?" + "text= " + text;
 }
+
+function btnClickHandler() {
+  var inputTxt = txtInput.value;
+  fetch(getTranslationUrl(inputTxt))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedTxt = json.contents.translated;
+      outputDiv.innerText = translatedTxt;
+    })
 btnTranslate.addEventListener("click", btnClickHandler);
